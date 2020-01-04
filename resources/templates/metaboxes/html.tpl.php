2bee719b1ccc87d92a6cc8527b4ba0f4bd1b4b55
kansai-gaijin <windecker.lc@gmail.com>
2020-01-04 23:26:44 +0900

    added html meta field

--- resources/templates/metaboxes/html.tpl.php
+++ resources/templates/metaboxes/html.tpl.php
@@ -2,13 +2,13 @@
   $value = get_post_meta(get_the_ID(), $field_data['slug'], true);
 ?>
 
-<p class="meta-options jb-field-text">
+<div class="meta-options jb-field-html">
     <label class="field-title" for="<?= $field_data['slug']; ?>"><?= $field_data['name'] ?><?= ($field_data['required']) ? ' <small class="req">必須</small>' : '' ?></label>
     <?php
       wp_editor(
         $value ,
         $field_data['slug'],
-        array( "media_buttons" => true )
+        array( "media_buttons" => true, "textarea_name" => 'jbf[' . $field_data['slug'] . ']', )
       );
     ?>
-</p>
+</div>
